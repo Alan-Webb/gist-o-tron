8f321d511f60730671a649ce5c35cd46aca3d786
Alan Webb <webbalan91939@gmail.com>
2026-01-02 00:40:19 +0000

    Update bg

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
 	};
 
 	return (
-		<div className="min-h-screen bg-linear-to-br from-pink-950 via-stone-950 to-purple-900 flex flex-col items-center justify-center p-3 gap-6">
+		<div className="bg-cover bg-center bg-no-repeat bg-[url('../src/assets/blue-grid.webp')] min-h-screen bg-linear-to-br from-pink-950 via-stone-950 to-purple-900 flex flex-col items-center justify-center p-3 gap-6">
 			<div className="uppercase text-center bg-linear-to-r from-sky-500 via-pink-500 to-indigo-500 bg-clip-text text-transparent">
 				<h1 className="text-6xl sm:text-8xl">Gist-0-Tron</h1>
 				<p className="text-2xl sm:text-3xl">AI Text Summarizer</p>
